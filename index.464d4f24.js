c131ec8051f71aeb73262fc66da0ea72e10b239e
Rustavas <115837907+Rustavas@users.noreply.github.com>
2023-04-13 13:35:57 +0000

    Deploying to gh-pages from @ Rustavas/goit-js-hw-10@bcaec9ba61ab8ba7e478c3298deaf520aa90c325 🚀

---
+++ index.464d4f24.js
@@ -0,0 +1,2 @@
+document.getElementById("search-box").addEventListener("input",(function(){}));
+//# sourceMappingURL=index.464d4f24.js.map
---
+++ index.b10bb5b9.js
@@ -0,0 +1,2 @@
+({searchInput:document.getElementById("search-box")}).searchInput.addEventListener("input",(function(){}));
+//# sourceMappingURL=index.b10bb5b9.js.map
